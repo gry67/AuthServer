e940222c12676540feb075332fca7ce6346e183a
gry67 <117734055+gry67@users.noreply.github.com>
2024-02-16 16:13:30 +0300

    .

--- GryAuthServer.API/Validations/CreateUserDtoValidator.cs
+++ GryAuthServer.API/Validations/CreateUserDtoValidator.cs
@@ -7,7 +7,7 @@ namespace GryAuthServer.API.Validations
     {
         public CreateUserDtoValidator()
         {
-            //              Email Boş olamaz. Boş olur ise göster:       Bu bir Email adresidir.Email @ yoksa göster:        
+            //             Email Boş olamaz. Boş olur ise göster:       Bu bir Email adresidir.Email @ yoksa göster:        
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required").EmailAddress().WithMessage("Email is wrong");
 
             RuleFor(x => x.Password).NotEmpty().WithMessage("Password is required");
